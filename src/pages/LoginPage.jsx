d6441a958a39efe085b40b3e1f033250d4cc42ed
AlejandroEs04 <2004.estrada.lopez@gmail.com>
2024-05-23 13:58:36 -0600

    'Change on design main'

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import useAuth from '../hooks/useAuth';
 import useApp from '../hooks/useApp';
 import Spinner from '../components/Spinner';
 
 const LoginPage = () => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [bussiness, setBussiness] = useState("");
+
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [alerta, setAlerta] = useState(null)
+
+  const [newAccount, setNewAccount] = useState(false)
 
   const { loading, setLoading } = useApp();
   const { auth, setAuth } = useAuth();
@@ -41,41 +49,67 @@ const LoginPage = () => {
   }
 
   return (
-    <div className='container my-5 allScreenHeight'>
-      <div className="row g-0 shadow-lg">
-        <form className={`col-md-6 bg-light p-5`}>
-          <div>
-            <h1 className='text-start'>Iniciar Sesión</h1>
-            <p>Ingrese sus licencias para iniciar sesión</p>
+    <div className='container my-5 py-4'>
+      <div className='d-flex justify-content-center'>
+        {newAccount ? (
+          <form className={`p-5 shadow maxWidthForm bg-light w-100`}>
+            <div>
+              <h1 className='text-start'>Solicitar Accesso</h1>
+              <p>Complete el formulario para solicitad acceso al sistema</p>
 
-            {alerta && (
-              <p className='alert alert-danger'>{alerta.msg}</p>
-            )}
-          </div>
+              {alerta && (
+                <p className='alert alert-danger'>{alerta.msg}</p>
+              )}
+
+            </div>
+
+            <label htmlFor="name" className='form-label'>Nombre(s)</label>
+            <input type="text" value={name} placeholder='Nombre(s)' onChange={e => setName(e.target.value)} id="nombre" className='form-control' />
+            
+            <label htmlFor="lastName" className='form-label mt-2'>Apellido(s)</label>
+            <input type="text" value={lastName} placeholder='Apellido(s)' onChange={e => setLastName(e.target.value)} id="lastName" className='form-control' />
+            
+            <label htmlFor="email" className='form-label mt-2'>Correo</label>
+            <input type="email" value={email} placeholder='Ej. correo@correo.com' onChange={e => setEmail(e.target.value)} id="email" className='form-control' />
           
-          <div className={`d-flex flex-column mt-1`}>
-            <label htmlFor="userName" className='form-label'>Nombre de usuario</label>
-            <input type="text" value={userName} placeholder='Nombre de usuario' onChange={e => setUserName(e.target.value)} id="userName" className='form-control' />
-          </div>
-          <div className={`d-flex flex-column mt-2`}>
-            <label htmlFor="password" className='form-label'>Contraseña</label>
-            <input type="password" value={password} placeholder='Contraseña' onChange={e => setPassword(e.target.value)} id="password" className='form-control' />
-          </div>
+            <label htmlFor="bussiness" className='form-label mt-2'>Nombre de la empresa</label>
+            <input type="text" value={bussiness} placeholder='Nombre de la empresa' onChange={e => setBussiness(e.target.value)} id="bussiness" className='form-control' />
 
-          {loading ? (
-            <Spinner />
-          ) : (
-            <button type="button" onClick={() => handleLogin()} className={`btnPrimaryBg mt-3`}>Iniciar Sesión</button>
-          )}
+            <button type='button' onClick={() => console.log("Enviando")} className='btn bgPrimary mt-3'>Solicitar Acceso</button>
 
-        </form>
+            <p className='mt-3 mb-0'>¿Ya tienes cuenta? <button type='button' className='text-primary px-0' onClick={() => setNewAccount(false)}>Iniciar Sesión</button></p>
+            <p className='mt-1'>¿Olvidaste tu password? <Link to={"/"} className='text-primary px-0 text-decoration-none'>Solicitad Cambio</Link></p>
+          </form>
+        ) : (
+          <form className={`p-5 shadow maxWidthForm bg-light w-100`}>
+            <div>
+              <h1 className='text-start'>Iniciar Sesión</h1>
+              <p>Ingrese sus licencias para iniciar sesión</p>
 
-        <div className='col-md-6 bg bgPrimary p-5 text-center'>
-            <h4 className='fs-3 fw-bold'>Bienvenido al sistema de Mopal Grupo</h4>
-            <p className='fs-5'>Si desea tener acceso al sistema, favor de ingresar la siguiente informacion</p>
+              {alerta && (
+                <p className='alert alert-danger'>{alerta.msg}</p>
+              )}
+            </div>
+            
+            <div className={`d-flex flex-column mt-1`}>
+              <label htmlFor="userName" className='form-label'>Nombre de usuario</label>
+              <input type="text" value={userName} placeholder='Nombre de usuario' onChange={e => setUserName(e.target.value)} id="userName" className='form-control' />
+            </div>
+            <div className={`d-flex flex-column mt-2`}>
+              <label htmlFor="password" className='form-label'>Contraseña</label>
+              <input type="password" value={password} placeholder='Contraseña' onChange={e => setPassword(e.target.value)} id="password" className='form-control' />
+            </div>
 
-            <button className='btn btn-dark'>Solicitar Acceso</button>
-        </div>
+            {loading ? (
+              <Spinner />
+            ) : (
+              <button type="button" onClick={() => handleLogin()} className={`btnPrimaryBg mt-3`}>Iniciar Sesión</button>
+            )}
+
+            <p className='mt-3 mb-0'>¿Aun no tienes cuenta? <button type='button' className='text-primary px-0' onClick={() => setNewAccount(true)}>Solicitar Acceso</button></p>
+            <p className='mt-1'>¿Olvidaste tu password? <Link to={"/"} className='text-primary px-0 text-decoration-none'>Solicitad Cambio</Link></p>
+          </form>
+        )}
       </div>
     </div>
   )
